329acee00aaede442740317e07e878384baf5573
kenneth <vadeveloper@gmail.com>
2025-06-01 19:12:32 -0700

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -106,7 +106,7 @@ export function Footer() {
                 <svg className="w-5 h-5 mr-2 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <span>(415) 872-7100</span>
+                <a href="tel:14158727100" className="hover:text-blue-400 transition-colors">(415) 872-7100</a>
               </li>
             </ul>
           </div>
